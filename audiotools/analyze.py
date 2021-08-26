4b55229c1b087148c40c69a6c75a9887b61cf03f
oilcake <oilpie@gmail.com>
2021-08-27 00:55:54 +0300

    wip

--- audiotools/analyze.py
+++ audiotools/analyze.py
@@ -2,19 +2,15 @@ import os
 from aubio import source, onset
 
 
-def find_onsets(filename, threshold=2.1):
-
-    print()
+def find_onsets(filename, threshold=2.1, window=32):
 
     if os.path.isfile(filename):
         print('looking for onsets in', filename)
     else:
         print("Can't find your file")
 
-    print()
-
-    win_s = 16  # fft size
-    hop_s = win_s // 2  # hop size
+    win_s = window  # fft size
+    hop_s = win_s // 4  # hop size
     samplerate = 0
     o = onset("default", win_s, hop_s, samplerate)
 
--- transientslicer.py
+++ transientslicer.py
@@ -30,7 +30,7 @@ makedir(path_out)
 files = get_files(path_in)
 
 for file in files:
-    onsets = find_onsets(file)
+    onsets = find_onsets(file, threshold=1.5, window=1024)
     sample = source(file, samplerate=0, hop_size=64)
     x, sr = librosa.load(file)
     plt.figure(figsize=(18, 3))
@@ -39,13 +39,13 @@ for file in files:
     print()
     print('samplerate = ', sample.samplerate)
     for onset in onsets:
-        wave_x = librosa.samples_to_time(onset, sr=sr*2)
-        plt.plot(wave_x, 0.2, marker=9)
+        onset_time = librosa.samples_to_time(onset, sr=sr*2) # converts onset time from samples to ms
+        plt.plot(onset_time, 0.2, marker=9)
         note = Note(sample, onset)
         print(file, 'onset = ', onset)
         print('duration = ', note.duration)
         print('max value = ', note.max)
         duration_ms = librosa.samples_to_time(note.duration, sr=sr*2)
-        plt.plot(wave_x + duration_ms, 0.2, marker=2)
+        plt.plot(onset_time + duration_ms, 0.2, marker=2)
     plt.show()
     sample.close()
